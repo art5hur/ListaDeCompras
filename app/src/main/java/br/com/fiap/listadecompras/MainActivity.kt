d969eb31eac2db0754277860f8dca37ed3db1b99
Arthur Miranda <119464045+art5hur@users.noreply.github.com>
2024-05-07 13:39:08 -0300

    Update MainActivity.kt

--- app/src/main/java/br/com/fiap/listadecompras/MainActivity.kt
+++ app/src/main/java/br/com/fiap/listadecompras/MainActivity.kt
@@ -16,7 +16,8 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.fiap.listadecompras.ui.theme.ListaDeComprasTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    @SuppressLint("MissingInflatedId")
+      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
