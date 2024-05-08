17e1b3e780a3410b6aa91e9da1e5ec6254cd4805
Arthur Miranda <119464045+art5hur@users.noreply.github.com>
2024-05-08 13:59:48 -0300

    Update ItemModel.kt

--- app/src/main/java/br/com/fiap/listadecompras/ItemModel.kt
+++ app/src/main/java/br/com/fiap/listadecompras/ItemModel.kt
@@ -1,3 +1,12 @@
 package br.com.fiap.listadecompras
+/*
+  Um classe modelo de dados para representar um item na lista de compras.
+ */
 
+//data - Ao colocar o “data” o Kotlin entende que é uma modelagem de BD;
+
+/*
+  Constrói um novo ItemModel
+     name: O nome do item.
+*/
 data class ItemModel(val name: String)
